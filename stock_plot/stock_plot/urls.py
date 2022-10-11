705b23e8e13269329a26347849955fd8e954db14
anirudhasj441 <anirudhasj441@gmail.com>
2022-10-11 08:33:07 +0530

    dev: add home app

--- stock_plot/stock_plot/urls.py
+++ stock_plot/stock_plot/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('plots.urls'))
+    path('', include('home.urls')),
+    path('plots/', include('plots.urls'))
 ]
