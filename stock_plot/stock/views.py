9457f05658c25d3a6dc269a82036659ca010ea9c
anirudhasj441 <anirudhasj441@gmail.com>
2022-10-25 17:03:55 +0530

    dev: add news api to stock page

--- stock_plot/stock/views.py
+++ stock_plot/stock/views.py
@@ -1,5 +1,8 @@
-from locale import currency
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
+from django.http import JsonResponse
+import requests
+import json
 
 # Create your views here.
 
@@ -14,3 +17,31 @@ def stock(request):
     }
 
     return render(request, "stock/stock.html", params)
+
+
+#APIs
+
+@csrf_exempt
+def getNews(request):
+    try:
+        response = {}
+        data = json.loads(request.body)
+        url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/news/v2/list"
+        params = {
+            "region": "IN",
+            "snippetCount": 4,
+            "s": data["symbol"]
+        }
+        headers = {
+            "X-RapidAPI-Key": "7517de7ee2mshc50158a550fd525p1ee8c2jsnabb54b407710",
+            "X-RapidAPI-Host": "apidojo-yahoo-finance-v1.p.rapidapi.com"
+        }
+        results = requests.request("POST", url, headers=headers, params=params).json()
+        response["results"] = results
+    except Exception as e:
+        response["status"] = "failed"
+        response["message"] = "Error: " + str(e)
+    else:
+        response["status"] = "status"
+    finally:
+        return JsonResponse(response, safe=False)
